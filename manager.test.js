7d0199e4f33bf14b7f64898ad8d33d38dfa14838
zhughes3 <zachary.hughes@gopuff.com>
2020-08-25 09:16:29 -0400

    flush promises from https://stackoverflow.com/questions/52177631/jest-timer-and-promise-dont-work-well-settimeout-and-async-function -- still failing

--- manager.test.js
+++ manager.test.js
@@ -17,12 +17,16 @@ test("cacheEntry gets deleted", async () => {
     expect(cacheValue).toEqual(cacheEntry.value)
 
     // part 3: test that cache entry has been deleted after expiration time has passed
-    jest.advanceTimersByTime(expirationDurationSeconds * 1000) // forceExpiration
-    await Promise.resolve()
+    jest.advanceTimersByTime((expirationDurationSeconds + 1) * 1000) // forceExpiration
+    await flushPromises()
 
     let expiredCacheValue = await Manager.getKey(cacheEntry.key)
     // since the expire event never gets emitted, and the expire function never runs,
     // the cache entry never gets deleted and still has the original value
     expect(expiredCacheValue).toEqual(undefined)
 })
+
+function flushPromises() {
+    return new Promise(resolve => setImmediate(resolve));
+}
 
